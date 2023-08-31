17f3ec2e9e55e4f320f1602456238fb537fe65a5
Paul Alexei <paul.allexei@gmail.com>
2023-08-31 16:33:34 +0300

    Added HELP and ABOUT

--- client/src/components/ModalAbout.js
+++ client/src/components/ModalAbout.js
@@ -6,9 +6,32 @@ const ModalAbout = ({ setModalOpen}) => {
     <>
       <div className="modalBackground">
         <div className="modalContainer">
-          <div className="title">About</div>
           <div className="body-text">
-           
+          <h4 style={{ textAlign: 'center' }}>UNIVERSITATEA "OVIDIUS" DIN CONSTANŢA FACULTATEA DE MATEMATICĂ ȘI INFORMATICĂ</h4>
+          <br />
+          <br />
+          <br />
+          <br />
+              <h2 style={{ textAlign: 'center' }}>THE USE OF BLOCKCHAIN TECHNOLOGY IN DOCUMENT MANAGEMENT</h2>
+              <br />
+              <br /><br />
+          <br />
+          <br />
+          <br />
+          <br />
+            <h4 style={{ textAlign: 'left' }}>COORDONATOR ŞTIINŢIFIC</h4>
+            <h4 style={{ textAlign: 'left' }}>Conf. Univ. Dr. PUCHIANU CRENGUȚA-MĂDĂLINA</h4>
+            <br />
+          <br />
+          <br />
+                <h4 style={{ textAlign: 'right' }}>ABSOLVENT</h4>
+                <h4 style={{ textAlign: 'right' }}>ALEXEI PAUL</h4>
+                <h5 style={{ textAlign: 'right' }}>paul.allexei@gmail.com</h5>
+                <br />
+          <br />
+          <br />
+                <h4 style={{ textAlign: 'center' }}>2023</h4>
+                
           </div>
 
           <div className="footer">
--- client/src/components/ModalHelp.js
+++ client/src/components/ModalHelp.js
@@ -1,14 +1,50 @@
 import { useEffect } from "react";
 import "./Modal.css";
 
-const ModalHelp = ({ setModalOpen}) => {
+const ModalHelp = ({ setModalOpen }) => {
   return (
     <>
       <div className="modalBackground">
         <div className="modalContainer">
-          <div className="title">Help</div>
-          <div className="body-text">
-           
+          <div className="title"></div>
+          <div className="body-text justify-text">
+            <h4 className="instruction-title"></h4>
+            <h5 className="instruction-title">Connecting to Sepolia Testnet on MetaMask:</h5>
+            <p>Follow the instructions on <a className="link" href="https://www.alchemy.com/overviews/how-to-add-sepolia-to-metamask" target="_blank" rel="noopener noreferrer">this guide</a> to add Sepolia to your MetaMask wallet.</p>
+            
+            <h5 className="instruction-title">Getting Free SepoliaETH:</h5>
+            <ol className="instruction-list">
+              <li>Visit <a className="link" href="https://sepoliafaucet.com/" target="_blank" rel="noopener noreferrer">Sepolia Faucet</a></li>
+              <li>Follow instructions to get free SepoliaETH.</li>
+            </ol>
+
+            <h5 className="instruction-title">Uploading a File:</h5>
+            <ol className="instruction-list">
+              <li>Click "Upload" and select the file you want to upload.</li>
+              <li>Confirm payment in MetaMask.</li>
+              <li>Wait for notification confirming upload is complete.</li>
+              <li>File will appear in your collection.</li>
+            </ol>
+
+            <h5 className="instruction-title">Sharing Files:</h5>
+            <ol className="instruction-list">
+              <li>Click "Share" in the app.</li>
+              <li>Enter the recipient's address and then click "OK".</li>
+              <li>Recipient addresses will appear in a dropdown UI.</li>
+            </ol>
+
+            <h5 className="instruction-title">Accessing Shared Files:</h5>
+            <ol className="instruction-list">
+              <li>Enter the sharer's address in the field at the bottom.</li>
+              <li>Click "Get Data".</li>
+            </ol>
+
+            <h5 className="instruction-title">Viewing Your Own Files:</h5>
+            <ol className="instruction-list">
+              <li>Remove any address from the field at the bottom.</li>
+              <li>Click "Get Data".</li>
+            </ol>
+
           </div>
 
           <div className="footer">
@@ -26,4 +62,5 @@ const ModalHelp = ({ setModalOpen}) => {
     </>
   );
 };
+
 export default ModalHelp;
